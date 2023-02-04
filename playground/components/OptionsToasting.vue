d679f844888bf0e933d13ab2b5d2f84ac1969521
Vincent Girard <vinccool96@gmail.com>
2023-02-04 16:45:03 -0500

    options api

--- playground/components/OptionsToasting.vue
+++ playground/components/OptionsToasting.vue
@@ -2,28 +2,31 @@
   <button @click="showToast">Show toast</button>
 </template>
 
-<script setup lang="ts">
-import { useNuxtApp } from "#app"
+<script lang="ts">
+import { defineNuxtComponent } from "#app"
 import { POSITION, TYPE } from "vue-toastification"
-const { $toast } = useNuxtApp()
 
-function getRandom<T>(list: Array<T>): T {
-  return list[Math.floor((Math.random() * 10) % list.length)]
-}
+export default defineNuxtComponent({
+  methods: {
+    getRandom<T>(list: Array<T>): T {
+      return list[Math.floor((Math.random() * 10) % list.length)]
+    },
 
-function randomType(): TYPE {
-  return getRandom(Object.values(TYPE))
-}
+    randomType(): TYPE {
+      return this.getRandom(Object.values(TYPE))
+    },
 
-function randomPosition(): POSITION {
-  return getRandom(Object.values(POSITION))
-}
+    randomPosition(): POSITION {
+      return this.getRandom(Object.values(POSITION))
+    },
 
-function randomMessage(): string {
-  return getRandom(["Hello", "I'm a toast!", "Oops!", "Hi :)"])
-}
+    randomMessage(): string {
+      return this.getRandom(["Hello", "I'm a toast!", "Oops!", "Hi :)"])
+    },
 
-function showToast(): void {
-  $toast(randomMessage(), { type: randomType(), position: randomPosition() })
-}
+    showToast(): void {
+      this.$toast(this.randomMessage(), { type: this.randomType(), position: this.randomPosition() })
+    },
+  },
+})
 </script>
