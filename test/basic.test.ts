adebd6295d44b12e21511743b081ba6efc60c1e4
Vincent Girard <vinccool96@gmail.com>
2023-02-05 14:13:45 -0500

    Tests

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -1,17 +1,31 @@
 import { fileURLToPath } from "node:url"
 
-import { describe, it, expect } from "vitest"
-import { setup, $fetch } from "@nuxt/test-utils"
+import { describe, expect, it } from "vitest"
+import { createPage, setup } from "@nuxt/test-utils"
 
-describe("ssr", async () => {
+describe("ssr", async function () {
   await setup({
     // @ts-ignore
     rootDir: fileURLToPath(new URL("./fixtures/basic", import.meta.url)),
   })
 
-  it("renders the index page", async () => {
+  it("renders the index page", async function () {
     // Get response to a server-rendered page with `$fetch`.
-    const html: string = await $fetch("/")
-    expect(html.length).toEqual(713)
+    const page = await createPage("/")
+    const html: string = await page.content()
+    expect<string>(html).toContain("<div>basic</div>")
+  })
+
+  it("should contain the div used to show the toasts", async function () {
+    const verticalPositions = ["top", "bottom"]
+    const horizontalPositions = ["left", "center", "right"]
+
+    const html: string = await (await createPage("/")).content()
+
+    for (const vPos of verticalPositions) {
+      for (const hPos of horizontalPositions) {
+        expect<string>(html).toContain(`<div class="Vue-Toastification__container ${vPos}-${hPos}"></div>`)
+      }
+    }
   })
 })
